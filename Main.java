dd4d917263a1bf1fa54dab34cf0aa655e0ae6b48
DimaVakarchuk <DeepVibes@yandex.ru>
2020-09-04 21:35:08 +0300

    Initial new Main.java

--- Main.java
+++ Main.java
@@ -1,15 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        boolean positivebalance = true;
-        int openingbalance = 500;
-        int newbalance = 2500;
+        boolean positiveBalance = true;
+        int openingBalance = 500;
+        int newBalance = 2500;
         int bonus = 2500 / 100;
-        int finalbonus = newbalance / 100;
+        if (newBalance > 1000) {
+            bonus = newBalance / 100;
+        }
 
-
-        System.out.println(openingbalance + newbalance + bonus);
-        System.out.println(finalbonus);
-
+        System.out.println("Общий балланс с учетом бонусов: " + (openingBalance + newBalance + bonus));
+        System.out.println("Начисленный бонус: " + bonus);
     }
 }
-
