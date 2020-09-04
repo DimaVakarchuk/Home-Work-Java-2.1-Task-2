dba97d72774ba9dfa587576a61609b6eff049231
DimaVakarchuk <DeepVibes@yandex.ru>
2020-09-04 20:12:16 +0300

    Initial new code

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        boolean positivebalance = true;
+        int openingbalance = 500;
+        int newbalance = 2500;
+        int bonus = 2500 / 100;
+        int finalbonus = newbalance / 100;
+
+
+        System.out.println(openingbalance + newbalance + bonus);
+        System.out.println(finalbonus);
+
+    }
+}
+
